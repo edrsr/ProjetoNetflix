7a9c67290ddf4b5034507f94658598368abfd438
edrsr <ednaldomendesdearaujo@gmail.com>
2020-12-04 14:50:15 -0300

    Primeiro Commit

---
+++ js/scriptJs.js
@@ -0,0 +1,5 @@
+function clicou(){
+    document.getElementById("informacoes");
+    console.log(document.getElementById("informacoes").innerHTML ="Como policiais, Adam Mitchell, Nathan Hayes e seus parceiros estão confiantes e focados. Eles voluntariamente enfrentam o pior que o mundo pode oferecer. Serão eles capazes de encontrar uma maneira de servir e proteger aqueles que são mais valiosos? Corajosos... a honra começa em casa.");
+
+    }
